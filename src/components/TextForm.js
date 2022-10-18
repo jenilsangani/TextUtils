cec9791d603f26ab4631e37f16ce6b726246e30f
jenil sangani <jenilsangani2000@gmail.com>
2022-10-18 15:25:38 -0500

    added console

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
-    let newText = text.toUpperCase();
-    setText(newText);
-    console.log(handleUpClick);
+    let newTextUpperCase = text.toUpperCase();
+    setText(newTextUpperCase);
+    console.log("Text changed in Uppercase" + " : " + newTextUpperCase);
   };
   const handleLowClick = () => {
-    let newText1 = text.toLowerCase();
-    setText(newText1);
+    let newTextLowercase = text.toLowerCase();
+    setText(newTextLowercase);
+    console.log("Text changed in Lowercase" + " : " + newTextLowercase);
   };
   const handleonChange = (event) => {
     setText(event.target.value);
@@ -19,15 +20,37 @@ export default function TextForm(props) {
       <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value={text} onChange={handleonChange} id="myBox" rows="8"></textarea>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleonChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
         </div>
-        <button className="btn btn-primary" style={{ marginRight: 16 }} onClick={handleUpClick}> Convert to Uppercase </button>
+        <button
+          className="btn btn-primary"
+          style={{ marginRight: 16 }}
+          onClick={handleUpClick}
+        >
+          {" "}
+          Convert to Uppercase{" "}
+        </button>
 
-        <button className="btn btn-primary" style={{ marginRight: 16 }} onClick={handleLowClick}> Convert to Lowercase </button>
+        <button
+          className="btn btn-primary"
+          style={{ marginRight: 16 }}
+          onClick={handleLowClick}
+        >
+          {" "}
+          Convert to Lowercase{" "}
+        </button>
       </div>
       <div className="container my-3">
         <h1>Your text summary</h1>
-        <p>{text.split(" ").length} Words and {text.length} Characters</p>
+        <p>
+          {text.split(" ").length} Words and {text.length} Characters
+        </p>
       </div>
     </>
   );
