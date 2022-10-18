9d6c44272d1eb7d73081c84bf3466b92255bd32f
jenil sangani <jenilsangani2000@gmail.com>
2022-10-18 15:14:25 -0500

    added text summary

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,6 +4,7 @@ export default function TextForm(props) {
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    console.log(handleUpClick);
   };
   const handleLowClick = () => {
     let newText1 = text.toLowerCase();
@@ -14,32 +15,20 @@ export default function TextForm(props) {
   };
   const [text, setText] = useState("Enter text here");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleonChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
-      </div>
-      <button
-        className="btn btn-primary"
-        style={{ marginRight: 16 }}
-        onClick={handleUpClick}
-      >
-        Convert to Uppercase
-      </button>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleonChange} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary" style={{ marginRight: 16 }} onClick={handleUpClick}> Convert to Uppercase </button>
 
-      <button
-        className="btn btn-primary"
-        style={{ marginRight: 16 }}
-        onClick={handleLowClick}
-      >
-        Convert to Lowercase
-      </button>
-    </div>
+        <button className="btn btn-primary" style={{ marginRight: 16 }} onClick={handleLowClick}> Convert to Lowercase </button>
+      </div>
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} Words and {text.length} Characters</p>
+      </div>
+    </>
   );
 }
