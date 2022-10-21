19141c4251540a774230783467de91c47201050f
jenil sangani <jenilsangani2000@gmail.com>
2022-10-20 23:51:26 -0500

    add about componets

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 export default function About() {
+  let myStyle = {
+    color: "white",
+    backgroundColor: "black",
+  };
+
   return (
-    <div className="container">
+    <div className="container" style={myStyle}>
       <h1 className="my-3">About Us</h1>
-      <div className="accordion" id="accordionExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingOne">
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
             <button
-              className="accordion-button"
+              class="accordion-button"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseOne"
@@ -20,11 +25,11 @@ export default function About() {
           </h2>
           <div
             id="collapseOne"
-            className="accordion-collapse collapse show"
+            class="accordion-collapse collapse show"
             aria-labelledby="headingOne"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
+            <div class="accordion-body">
               <strong>This is the first item's accordion body.</strong> It is
               shown by default, until the collapse plugin adds the appropriate
               classes that we use to style each element. These classes control
@@ -36,10 +41,10 @@ export default function About() {
             </div>
           </div>
         </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingTwo">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingTwo">
             <button
-              className="accordion-button collapsed"
+              class="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseTwo"
@@ -51,11 +56,11 @@ export default function About() {
           </h2>
           <div
             id="collapseTwo"
-            className="accordion-collapse collapse"
+            class="accordion-collapse collapse"
             aria-labelledby="headingTwo"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
+            <div class="accordion-body">
               <strong>This is the second item's accordion body.</strong> It is
               hidden by default, until the collapse plugin adds the appropriate
               classes that we use to style each element. These classes control
@@ -67,10 +72,10 @@ export default function About() {
             </div>
           </div>
         </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingThree">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThree">
             <button
-              className="accordion-button collapsed"
+              class="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseThree"
@@ -82,11 +87,11 @@ export default function About() {
           </h2>
           <div
             id="collapseThree"
-            className="accordion-collapse collapse"
+            class="accordion-collapse collapse"
             aria-labelledby="headingThree"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
+            <div class="accordion-body">
               <strong>This is the third item's accordion body.</strong> It is
               hidden by default, until the collapse plugin adds the appropriate
               classes that we use to style each element. These classes control
