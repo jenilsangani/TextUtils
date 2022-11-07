acb14748b7d6d87d36d7acb6caa79f729ec15880
jenil sangani <jenilsangani2000@gmail.com>
2022-11-06 23:01:10 -0500

    edit in app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -10,7 +11,8 @@ function App() {
       {/* set default */}
       {/* <Navbar /> */}
       <div className="container my-3">
-        <TextForm heading = "Enter your text to analyze"/>
+        <TextForm heading = "Enter your text to analyze below"/>
+        <About />
       </div>
     </>
   );
