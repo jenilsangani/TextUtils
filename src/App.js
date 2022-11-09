43d839c1cff413575753d78d2b0a4d4c789be3b5
jenil sangani <jenilsangani2000@gmail.com>
2022-11-09 00:17:46 -0500

    alert msg set

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
@@ -14,23 +15,26 @@ function App() {
     if (mode === "light") {
       setkMode("dark");
       document.body.style.backgroundColor = "#062446";
+      showAlert("Dark mode has been enabled", "success"); //Alert msg
     } else {
       setkMode("light");
       document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success"); //Alert msg    
     }
   };
-  console.log(mode);
+  console.log("Enabled" + " : " + mode + " " + "mode");    // for console
 
   // Alert
 
-  const [alert, setkAlert] = useState(null);
+  const [alert, setAlert] = useState(null);
   const showAlert = (message, type) => {
-    setkAlert({
+    setAlert({
       msg : message,
       type : type
     })
   }  
-  console.log("alert msg" + " : " + alert);
+  // console.log("alert msg" + " : " + alert);   // for console
+
   return (
     <>
       <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Alert(props) {
   return (
-    <div className="alert alert-warning alert-dismissible fade show" role="alert">
-      <strong>{props.Alert}</strong>
+    props.alert && <div className="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong>{props.alert.type}</strong> : {props.alert.msg}
       <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   );
